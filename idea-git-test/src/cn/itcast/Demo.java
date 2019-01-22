5672be6e84c13d8d05716385cd4735cdc1ebaf26
zhangsan <22424553374@qq.com>
2019-01-23 01:11:29 +0800

    tibnsig

--- idea-git-test/src/cn/itcast/Demo.java
+++ idea-git-test/src/cn/itcast/Demo.java
@@ -5,5 +5,12 @@ public class Demo {
         System.out.println("欢迎使用github");
         System.out.println("欢迎使用github");
         System.out.println("欢迎使用github");
+
+
+        int a =11;
+        for (int i=0;i<10;i++){
+            a++;
+        }
+        System.out.println(a);
     }
 }
