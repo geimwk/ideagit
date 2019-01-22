0a4206d5246bf8350980500a4101339109c69daf
zhangsan <22424553374@qq.com>
2019-01-23 00:55:15 +0800

    添加git到本地仓储

---
+++ idea-git-test/src/cn/itcast/Demo.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用github");
+        System.out.println("欢迎使用github");
+        System.out.println("欢迎使用github");
+    }
+}
